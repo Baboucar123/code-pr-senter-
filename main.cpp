b6d2db7cc5895f907ac36e64fe5938cbd429162a
Baboucar123 <coly.baboucar.ab@gmail.com>
2024-09-25 11:49:05 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,16 +6,16 @@ using namespace std;
 void afficherMenu(Compte& compte, Compte comptes[], int nombreComptes);
 int main() {
     // Création de 4 clients et de leurs comptes
-    Client client1("CIN12345", "Jean", "Dupont","07498589", 1234);
+    Client client1("CIN12345", "Baboucar", "COLY","07498589", 1234);
     Compte compte1(1001, 5000.0, client1);
 
-    Client client2("CIN67890", "Marie", "Durand","01457896", 5678);
+    Client client2("CIN67890", "Anas", "Elarbaoui","01457896", 5678);
     Compte compte2(1002, 3000.0, client2);
 
-    Client client3("CIN98765", "Paul", "Martin","04568789", 4321);
+    Client client3("CIN98765", "Ilyas", "Baibbout","04568789", 4321);
     Compte compte3(1003, 4000.0, client3);
 
-    Client client4("CIN54321", "Alice", "Bernard","06489716", 8765);
+    Client client4("CIN54321", "Seynabou", "Diouf","06489716", 8765);
     Compte compte4(1004, 2500.0, client4);
 
     Compte comptes[4] = {compte1, compte2, compte3, compte4};
